d199bd49ef65f59a6f93add3909730c40a4c11af
DeveloppeurprofessionnelEmmanuel <laurencinemmanuel17@gmail.com>
2026-07-17 11:05:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,55 +10,61 @@ let stepText = document.getElementById("stepText");
 let percent = document.getElementById("percent");
 
 
-function afficherEtape(){
+function afficherEtape() {
 
-    steps.forEach(function(step){
+    steps.forEach(function(step) {
         step.classList.remove("active");
     });
 
     steps[current].classList.add("active");
 
+
     let progression = Math.round(((current + 1) / steps.length) * 100);
 
     bar.style.width = progression + "%";
+
     stepText.innerHTML = "Étape " + (current + 1) + " sur 9";
+
     percent.innerHTML = progression + " %";
 
 
-    if(current === 0){
+    if (current === 0) {
         prev.style.display = "none";
     } else {
         prev.style.display = "block";
     }
 
 
-    if(current === steps.length - 1){
+    if (current === steps.length - 1) {
         next.innerHTML = "📩 Envoyer mon cahier des charges";
     } else {
         next.innerHTML = "Suivant →";
     }
+
 }
 
 
-next.onclick = function(){
 
-    if(current < steps.length - 1){
+next.onclick = function() {
+
+    if (current < steps.length - 1) {
 
         current++;
         afficherEtape();
 
     } else {
 
-        alert("Le formulaire est prêt à être envoyé");
+        alert("Navigation terminée");
 
     }
 
 };
 
 
-prev.onclick = function(){
 
-    if(current > 0){
+prev.onclick = function() {
+
+    if (current > 0) {
 
         current--;
         afficherEtape();
@@ -66,6 +72,7 @@ prev.onclick = function(){
     }
 
 };
+
 
 
 afficherEtape();
